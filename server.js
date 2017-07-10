7bfd2a382b73252c31903158c58b7220ea0e96d0
Matthew Drumheller <MattDrummy@gmail.com>
2017-07-10 12:06:23 -0600

    final commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var server = express();
+var drummy = require('./me.json');
 var port = 5500;
 server.get('/', serverResponse);
 server.get('/drummy', drummyResponse);
@@ -9,21 +10,28 @@ server.listen(port);
 
 function serverResponse(request, response) {
   // console.log(request);
-  response.send("<h1>It's my very first local server!</h1>");
+  response.send(`
+    <h1>It's my very first local server!</h1>
+    <p>Go to localhost:5500/drummy to find out more about me!
+  `);
 }
 
 function drummyResponse(request, response) {
   // console.log(request);
-  response.send("<h1>My name is Matt, but my friends call me Drummy</h1>");
+  response.json(drummy);
 }
 
 function aboutResponse(request, response) {
   // console.log(request);
-  response.send("<h1>I'm so hyped for this!</h1>")
+  response.send(`
+    <h1>I'm so hyped for this!</h1>
+    `)
 }
 
 function wrongResponse(request, response) {
-  console.log(request);
+  // console.log(request);
   response.status(404);
-  response.send("<h1>Shit dude, this page isn't even here!</h1>")
+  response.send(`
+    <h1>Shit dude, this page isn't even here!</h1>
+    `)
 }
